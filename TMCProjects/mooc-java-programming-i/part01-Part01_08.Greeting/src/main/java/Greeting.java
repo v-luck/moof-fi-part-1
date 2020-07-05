af8d7d5eaff5855e19abcb1afca947731e592e78
v-luck <bobbabots@gmail.com>
2020-07-04 19:07:35 -0700

    finished some more exercises

--- TMCProjects/mooc-java-programming-i/part01-Part01_08.Greeting/src/main/java/Greeting.java
+++ TMCProjects/mooc-java-programming-i/part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -5,6 +5,9 @@ public class Greeting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your name?");
+        String message = scanner.nextLine();
+        System.out.println("Hi " + message);
 
         // Write your program here
 
