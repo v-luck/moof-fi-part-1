39fdc5466e7b28d37c21c5892be14630a172683d
v-luck <bobbabots@gmail.com>
2020-07-07 02:02:58 -0700

    Incorporated part2

--- TMCProjects/mooc-java-programming-i/part04-Part04_10.Song/src/main/java/Main.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_10.Song/src/main/java/Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
-      
+    Scanner scanner = new Scanner(System.in);
+    Integer value = scanner.nextInt();
+    System.out.println(value * value);
+
 
   }
 }
