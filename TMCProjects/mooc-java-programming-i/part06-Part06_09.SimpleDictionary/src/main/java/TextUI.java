edd308b0fa882614092c73a249a36ccf5d71cdf8
v-luck <bobbabots@gmail.com>
2020-08-16 22:07:27 -0700

    finished exercise 9

--- TMCProjects/mooc-java-programming-i/part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
+++ TMCProjects/mooc-java-programming-i/part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
@@ -11,9 +11,18 @@ public class TextUI {
 
     public void start() {
         while (true) {
-            System.out.print("Command: ");
-            String word = scanner.nextLine();
-            if (this.addEqual(word)) {
+            System.out.print("Command:");
+            String command = scanner.nextLine();
+            if (this.addEqual(command)) {
+                this.addEqualWord();
+                continue;
+            }
+
+            if (this.searchEqual(command)) {
+                this.searchEqualWord();
+                continue;
+            }
+            if (this.endEqual(command)) {
                 System.out.println("Bye bye!");
                 break;
             }
@@ -21,13 +30,37 @@ public class TextUI {
         }
     }
 
-    public boolean endEqual(String word) {
-        return word.equals("end");
+    public boolean endEqual(String command) {
+        return command.equals("end");
     }
 
-    public boolean addEqual(String word) {
-        return word.equals("add");
+    public boolean addEqual(String command) {
+        return command.equals("add");
     }
+
+    public void addEqualWord() {
+        System.out.print("Word: ");
+        String word = scanner.nextLine();
+        System.out.print("Translation: ");
+        String translation = scanner.nextLine();
+        this.dictionary.add(word, translation);
+    }
+
+    public boolean searchEqual(String command) {
+        return command.equals("search");
+    }
+
+    public void searchEqualWord() {
+        System.out.println("To be translated: ");
+        String word = scanner.nextLine();
+        if (this.dictionary.translate(word) == null) {
+            System.out.println("Word " + word + " was not found");
+
+        } else {
+            System.out.println("Translation: " + this.dictionary.translate(word));
+        }
+    }
+
 
 }
 
