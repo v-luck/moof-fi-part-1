d6ecf729263d611e00614fdcc38012ae64384189
v-luck <bobbabots@gmail.com>
2020-08-27 19:54:53 -0700

    finished exercise 6

--- TMCProjects/mooc-java-programming-i/part07-Part07_06.GradeStatistics/src/main/java/GradeStatistics.java
+++ TMCProjects/mooc-java-programming-i/part07-Part07_06.GradeStatistics/src/main/java/GradeStatistics.java
@@ -49,6 +49,7 @@ public class GradeStatistics {
         return 100.0 *passingGradeList().size() / gradeList.size();
     }
 
+
     public void printGrades() {
         System.out.println("Point average (all): " + this.gradeAverage());
         if (passingGradeList().size() == 0) {
@@ -57,5 +58,43 @@ public class GradeStatistics {
             System.out.println("Point average (passing): " + this.passingGradeAverage());
         }
         System.out.println("Pass percentage: " + passingGradePercentage());
+        this.gradeDistribution();
+        System.out.println("Grade distribution:");
+        printingStarts(gradeDistribution());
     }
+
+    // put in userinterface
+    public ArrayList<Integer> gradeDistribution() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int grade : gradeList) {
+            if (grade < 50) {
+                arrayList.add(0);
+            } else  if (grade <60) {
+                arrayList.add(1);
+            } else if (grade < 70) {
+                arrayList.add(2);
+            } else if (grade < 80) {
+                arrayList.add(3);
+            } else if (grade < 90) {
+                arrayList.add(4);
+            } else if (grade >= 90) {
+                arrayList.add(5);
+            }
+        }
+        return arrayList;
+    }
+
+    public void printingStarts(ArrayList<Integer> arrayList) {
+        for (int i = 5; i >= 0; i --) {
+            System.out.print(i + ": ");
+            for (int value : arrayList) {
+                if (value == i) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+
 }
