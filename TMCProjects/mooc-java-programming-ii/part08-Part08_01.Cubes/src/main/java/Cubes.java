35e402ebdbce898363a3cdeb341ae17fff90d332
v-luck <bobbabots@gmail.com>
2020-09-16 23:07:00 -0700

    Completed part 8 1

--- TMCProjects/mooc-java-programming-ii/part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ TMCProjects/mooc-java-programming-ii/part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,15 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("end")) {
+                break;
+            }
+
+            Integer userValue = Integer.valueOf(userInput);
+            System.out.println(userValue * userValue * userValue);
+        }
 
     }
 }
