659ecf410634c4be668d131932fbd20b50a83c3f
v-luck <bobbabots@gmail.com>
2020-09-21 18:38:29 -0700

    completeded exercise 3

--- TMCProjects/mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ TMCProjects/mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,6 +5,11 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        Container container1 = new Container();
+        Container container2 = new Container();
+        UserInterface userInterface = new UserInterface(container1, container2, scan);
+        userInterface.start();
+
 
     }
 
--- TMCProjects/mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/userInterface.java
+++ TMCProjects/mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/userInterface.java
@@ -1,27 +1,50 @@
 import java.util.Scanner;
 
-public class userInterface {
-    private Container container;
+public class UserInterface {
+    private Container container1;
+    private Container container2;
     private Scanner scanner;
-    public userInterface(Container container, Scanner scanner) {
-        this.container = container;
+    public UserInterface(Container container1, Container container2, Scanner scanner) {
+        this.container1 = container1;
+        this.container2 = container2;
         this.scanner = scanner;
     }
 
     public void start() {
         while (true) {
+            print();
             System.out.print("> ");
-
             String input = scanner.nextLine();
             if (input.equals("quit")) {
                 break;
             }
-
+            command(input);
+            System.out.println();
         }
     }
 
     public void command(String command) {
-        String[] commandParameters = command.split(" ");
+        String[] commandParameters = command.split("(?<=\\D)(?=\\d)");
+        String commandCall = commandParameters[0];
+        Integer commandAmount = Integer.valueOf(commandParameters[1]);
 
+        switch (commandCall) {
+        case "add ":
+            container1.add(commandAmount);
+            break;
+        case "remove ":
+            container2.remove(commandAmount);
+            break;
+        case "move ":
+            Integer containerMove = container1.contains();
+            container1.remove(commandAmount);
+            Integer removeFromSecond = containerMove - container1.contains();
+            container2.add(removeFromSecond);
+            break;
+        }
+    }
+
+    public void print() {
+        System.out.println(String.format("First: %s\nSecond: %s", container1, container2));
     }
 }
