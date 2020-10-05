f8ff7129767a20e435bbf07d5f9bc5ea9267af48
v-luck <bobbabots@gmail.com>
2020-10-04 20:32:34 -0700

    finished exercise 6

--- TMCProjects/mooc-java-programming-ii/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ TMCProjects/mooc-java-programming-ii/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -1,10 +1,16 @@
-
 import java.util.HashMap;
 
 public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap <String, String> hashMap = new HashMap<>();
+
+        hashMap.put("matthew", "matt");
+        hashMap.put("michael", "mix");
+        hashMap.put("arthur", "artile");
+
+        System.out.println(hashMap.get("matthew"));
     }
 
 }
