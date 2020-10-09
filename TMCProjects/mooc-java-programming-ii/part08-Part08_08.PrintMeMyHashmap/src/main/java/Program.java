880e0c28637c82bfb115276851036acaa2cc9277
v-luck <bobbabots@gmail.com>
2020-10-08 22:25:38 -0700

    holy

--- TMCProjects/mooc-java-programming-ii/part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ TMCProjects/mooc-java-programming-ii/part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -1,10 +1,29 @@
-
 import java.util.HashMap;
 
 public class Program {
 
     public static void main(String[] args) {
-        // Test your program here!
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("f.e", "for example");
+        hashMap.put("etc.", "and so on");
+        hashMap.put("i.e.", "more precisely");
+
+        printKeys(hashMap);
+    }
+
+    public static void printKeys(HashMap<String, String> hashMap) {
+        for (String key : hashMap.keySet()) {
+            System.out.println(key);
+        }
+    }
+
+    public static void printValuesOfKeysWhere(HashMap<String, String> hashMap, String text) {
+        for (String key : hashMap.keySet()) {
+            if (!key.contains(text)) {
+                continue;
+            }
+            System.out.println(key);
+        }
     }
 
 }
