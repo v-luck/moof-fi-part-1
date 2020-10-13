a046cf4c1cdc662d9335c94be3244882b552681e
v-luck <bobbabots@gmail.com>
2020-10-13 09:24:51 -0700

    Completed exercise 9

--- TMCProjects/mooc-java-programming-ii/part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
+++ TMCProjects/mooc-java-programming-ii/part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
@@ -1,10 +1,27 @@
-
 import java.util.HashMap;
 
 public class Program {
 
     public static void main(String[] args) {
-        // Test your program here!
+        HashMap<String, Book> hashmap = new HashMap<>();
+        hashmap.put("sense", new Book("Sense and Sensibility", 1811, "..."));
+        hashmap.put("prejudice", new Book("Pride and prejudice", 1813, "...."));
+        printValues(hashmap);
+        printValueIfNameContains(hashmap, "Pride");
+    }
+
+    public static void printValues(HashMap<String, Book> hashMap) {
+        for (Book book : hashMap.values()) {
+            System.out.println(book);
+        }
+    }
+
+    public static void printValueIfNameContains(HashMap <String, Book> hashMap, String text) {
+        for (Book book : hashMap.values()) {
+            if (book.getName().contains(text)) {
+                System.out.println(book);
+            }
+        }
     }
 
 }
